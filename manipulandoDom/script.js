2ee3fc97c845cf1e8af38df4512631d097eb2c30
rafael <rafahreispc@gmail.com>
2022-07-15 08:38:46 -0300

    feat: added exercises

--- manipulandoDom/script.js
+++ manipulandoDom/script.js
@@ -30,3 +30,27 @@ console.log(linguagem);
 
 const tamanho = innerWidth;
 console.log(tamanho);
+
+
+
+// trazendo todas as 7 imagens
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
+
+// trazendo so as imagens que tem os animais (6 imagens)
+const imgAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log( imgAnimais);
+
+// trazendo links internos
+const links = document.querySelectorAll('[href^="#"]')
+console.log(links.innerHTML);
+
+// selecionar primeiro h2 dentro de .animais desc
+
+const h2animais = document.querySelector('.animaisDesc h2');
+console.log(h2animais);
+
+// selecionando ultimo p do site 
+
+const ultimoP = document.querySelectorAll('p');
+console.log(ultimoP[ultimoP.length - 1]);
